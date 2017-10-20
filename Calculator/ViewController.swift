a7f76c4b9e8b6dd1cdbf69ef4166fe21130586b6
latne1ec <latne1ec@cmich.edu>
2017-10-19 20:40:04 -0400

    First commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Evan Latner on 10/17/17.
+//  Copyright © 2017 levellabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Implicitely Unwrapped
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    // Computed Property
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematecalSymbol = sender.currentTitle {
+            switch mathematecalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+}
+
