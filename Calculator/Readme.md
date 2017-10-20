e8aa7ac395c3091468f94b317f02f72c51107918
latne1ec <latne1ec@cmich.edu>
2017-10-19 22:53:15 -0400

    MVC, Responsive UI

---
+++ Calculator/Readme.md
@@ -0,0 +1,14 @@
+Classes vs Structs
+
+• Classes have inheritance, Structs do not
+• Classes live in the heap and have pointers pointing to them
+• Structs do not live in the heap, and are passed around by copying them
+• Structs automatically get an initializer that will initialize all uninitialized vars
+• With structs, you must mark methods "mutating" to let it know that value can change
+• The swift version of a hash table is a dictionary
+• Closures are functions embedded right in a line of code
+•
+•
+•
+•
+
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,17 +36,18 @@ class ViewController: UIViewController {
         }
     }
     
+    private var brain: CalculatorBrain = CalculatorBrain()
+    
     @IBAction func performOperation(_ sender: UIButton) {
-        userIsInTheMiddleOfTyping = false
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
         if let mathematecalSymbol = sender.currentTitle {
-            switch mathematecalSymbol {
-            case "π":
-                displayValue = Double.pi
-            case "√":
-                displayValue = sqrt(displayValue)
-            default:
-                break
-            }
+            brain.performOperation(mathematecalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
         }
     }
 }
